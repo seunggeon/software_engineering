81a0674c949d5ec90ee25c67894a9f0fd4483f5f
seunggeon <tmdrjs9555@naver.com>
2023-03-29 00:04:13 +0900

    delete multiplication of 9

--- HW1_multiplication_tables.py
+++ HW1_multiplication_tables.py
@@ -1,7 +1,5 @@
 num = int(input("input a number for generating multiplication tables: "))
 
-for i in range(1, 10):
-    print(num, '*', i, '=', num*i)
 
 for j in range(10,20):
     print(num, '*', j, '=', num*j)
